49d23461bb99dffd41ec484ab12e02ecc5ceb34e
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-19 17:33:54 +0100

    Fix stripe payment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['boutique-ado-eco-47b3b6f3011f.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['boutique-ado-eco-47b3b6f3011f.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
